4fac25ead6a248b4928b152ea42a88cac168a980
Avinash Verma <eyedagger@gmail.com>
2016-02-18 18:30:09 +0530

    Improvements

--- src/snake-game.js
+++ src/snake-game.js
@@ -4,35 +4,42 @@ var SnakeGame = function (options) {
 	var WIDTH = options.width || 800,
 		HEIGHT = options.height || 600,
 		ELEM = document.getElementById(options.canvas),
-		BLOCK_SIZE = options.blockSize || 10,
+		BLOCK_SIZE = options.blockSize || 5,
 		SNAKE_SIZE = options.snakeSize || 10;
 
 	var context = ELEM.getContext('2d'),
 		snake = [];
 
+	function Coordinate(x, y) {
+		if (typeof x === typeof undefined || typeof y === typeof undefined) {
+			throw new Error("Both x and y should be passed Coordinate class");
+		}
+		this.x = x;
+		this.y = y;
+	}
+
 	function initializeSnake() {
 		for (var i = 0; i < SNAKE_SIZE; i++) {
-			snake.push({
-				x: i,
-				y: 0
-			})
+			var c = new Coordinate(i, 0);
+			snake.push(c);
 		}
 	}
 
 	function drawSnake() {
+
 		context.clearRect(0, 0, WIDTH, HEIGHT);
 		context.fillStyle = '#fff';
 		context.fillRect(0, 0, WIDTH, HEIGHT);
-		for (var i = 0; i < snake.length; i++) {
-			var pos = snake[i];
 
+		for (var i = 0; i < snake.length; i++) {
+			var c = snake[i]
 			context.fillStyle = "#000";
-			context.fillRect(pos.x * BLOCK_SIZE, pos.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+			context.fillRect(c.x * BLOCK_SIZE, c.y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
 		}
 	}
 
 	var initCanvas = function () {
-		if(ELEM === null) {
+		if (ELEM === null) {
 			throw new Error("Could not find the target Canvas : " + options.canvas);
 		}
 
